8abaf3705a60f768cecb46dd8beacd28df4d8472
swAAn01 <jaeswan@outlook.com>
2024-02-14 14:38:36 -0600

    1.0.0

---
+++ ReverbRemoval/Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ReverbRemoval
+{
+    [HarmonyPatch(typeof(OccludeAudio))]
+    class OccludeAudioPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("Update")]
+        static void UpdatePrefix(ref OccludeAudio __instance)
+        {
+            __instance.useReverb = false;
+            if (__instance.gameObject.GetComponent<AudioReverbFilter>())
+                __instance.gameObject.GetComponent<AudioReverbFilter>().enabled = false;
+        }
+    }
+}
---
+++ ReverbRemoval/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ReverbRemoval
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        
+        private void Awake()
+        {
+            Logger.LogInfo("Loading a mod by swAAn\n\n" +
+                "                                    _\n" +
+                "                                ,-\"\" \"\".\n" +
+                "                              ,'  ____  `.\n" +
+                "                            ,'  ,'    `.  `._\n" +
+                "   (`.         _..--.._   ,'  ,'        \\    \\\n" +
+                "  (`-.\\    .-\"\"        \"\"'   /          (  d _b\n" +
+                " (`._  `-\"\" ,._             (            `-(   \\\n" +
+                " <_  `     (  <`<            \\              `-._\\\n" +
+                "  <`-       (__< <           :\n" +
+                "   (__        (_<_<          ;\n" +
+                "    `~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                );
+
+            var harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
